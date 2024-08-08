23a91d6dc3c94f839f8b3ade219964509e95fe40
GlitchManu <manukumar0024@gmail.com>
2024-08-09 01:19:09 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const express = require("express");
+const path = require("path");
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+const Blog = require("./models/blog");
+
+
+PORT = process.env.PORT || 8000
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL).then(e => console.log("Mongodb Connected"));
+
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", async (req, res) => {
+    const allBlogs = await Blog.find({});
+    res.render("home", {
+      user: req.user,
+      blogs: allBlogs,
+    });
+  });
+
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
+app.listen(PORT, () => console.log(`Server is started at PORT: ${PORT}`));
